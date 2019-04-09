ecbbfc3b19c9723c8494eb498c25c8f506f844f9
Batooldania <49372647+Batooldania@users.noreply.github.com>
2019-04-09 23:40:41 +0500

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,16 @@
+class Task1
+{
+	public static void main(String[]args)
+	{
+	String name="dania batool";
+	double marks=95;
+	float percentage=93.5f;
+	char grade='A';
+	boolean status=ture;
+	char p="%";
+	System.out.println("name:"+name);
+	System.out.println("marks:"+marks);
+	System.out.println("percentage:"+percentage+p);
+	System.out.println("grade:"+grade);
+	System.out.println("status:pass"+status);
+	}
