41aad6c0969291672d0122df0d1330ee02242ff3
Batooldania <49372647+Batooldania@users.noreply.github.com>
2019-05-05 21:22:43 +0500

    Delete AdditionBool.java

--- oops lab/OOP-LAB 2/AdditionBool.java
+++
@@ -1,9 +0,0 @@
-class AdditionBool{
-	public static void main(String args[]){
-	int a=5;
-	int b=6;
-	int c=a+b;
-	System.out.println("It is :"+(c==11));
-	System.out.println("It is :"+(c!=11));
-	}
-}
